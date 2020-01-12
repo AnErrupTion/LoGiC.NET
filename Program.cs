d8cb7769d3d32bae9282cc9961621d1a0b90db1d
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2020-01-12 09:12:57 +0100

    1.3 : More optimizations and better protections

--- Program.cs
+++ Program.cs
@@ -15,7 +15,10 @@ namespace LoGiC.NET
         public static string FileExtension { get; set; }
 
         public static bool DontRename { get; set; }
+
         public static bool ForceWinForms { get; set; }
+
+        public static string FilePath { get; set; }
 
         static void Main(string[] args)
         {
@@ -23,10 +26,16 @@ namespace LoGiC.NET
             string path = Console.ReadLine();
 
             Console.WriteLine("Preparing obfuscation...");
+            if (!File.Exists("config.txt"))
+            {
+                Console.WriteLine("Config file not found, continuing without it.");
+                goto obfuscation;
+            }
             Parser p = new Parser() { ConfigFile = "config.txt" };
             ForceWinForms = bool.Parse(p.Read("ForceWinFormsCompatibility").ReadResponse().ReplaceSpaces());
             DontRename = bool.Parse(p.Read("DontRename").ReadResponse().ReplaceSpaces());
 
+            obfuscation:
             Module = ModuleDefMD.Load(path);
             FileExtension = Path.GetExtension(path);
 
@@ -45,13 +54,19 @@ namespace LoGiC.NET
             Console.WriteLine("Encrypting strings...");
             StringEncryption.Execute();
 
+            Console.WriteLine("Injecting Anti-Tamper...");
+            AntiTamper.Execute();
+
             Console.WriteLine("Watermarking...");
             Watermark.AddAttribute();
 
             Console.WriteLine("Saving file...");
+            FilePath = @"C:\Users\" + Environment.UserName + @"\Desktop\" + Path.GetFileNameWithoutExtension(path) + "_protected" +
+                FileExtension;
             ModuleWriterOptions opts = new ModuleWriterOptions(Module) { Logger = DummyLogger.NoThrowInstance };
-            Module.Write(@"C:\Users\" + Environment.UserName + @"\Desktop\" + Path.GetFileNameWithoutExtension(path) + "_protected" +
-                FileExtension, opts);
+            Module.Write(FilePath, opts);
+
+            if (AntiTamper.HasBeenTampered) AntiTamper.InjectMd5(FilePath);
 
             Console.WriteLine("Done! Press any key to exit...");
             Console.ReadKey();
