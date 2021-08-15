ecd7ceae93cc864f5c3e8074eebdb9bf0548b44e
AnErrupTion <ichicharka@gmail.com>
2021-08-15 12:55:38 +0200

    Improve and fix bugs in ControlFlow

--- Protections/ControlFlow.cs
+++ Protections/ControlFlow.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace LoGiC.NET.Protections
 {
-    public class ControlFlow
+    public class ControlFlow : Randomizer
     {
         public static void Execute()
         {
@@ -37,19 +37,22 @@ namespace LoGiC.NET.Protections
             for (int i = 0; i < method.Body.Instructions.Count; i++)
                 if (method.Body.Instructions[i].IsLdcI4())
                 {
-                    int numorig = Randomizer.Next();
-                    int div = Randomizer.Next();
+                    int numorig = Next();
+                    int div = Next();
 
                     int num = numorig ^ div;
 
+                    int lastIndex = Next(types.Length);
+                    Type randType = types[lastIndex];
+
                     Instruction nop = OpCodes.Nop.ToInstruction();
-                    Local local = new Local(method.Module.ImportAsTypeSig(typeof(InsufficientMemoryException)));
+                    Local local = new Local(method.Module.ImportAsTypeSig(randType));
                     Instruction localCode = OpCodes.Stloc.ToInstruction(local);
 
                     method.Body.Variables.Add(local);
 
                     method.Body.Instructions.Insert(i + 1, localCode);
-                    method.Body.Instructions.Insert(i + 2, Instruction.Create(OpCodes.Ldc_I4, method.Body.Instructions[i].GetLdcI4Value() - sizeof(uint)));
+                    method.Body.Instructions.Insert(i + 2, Instruction.Create(OpCodes.Ldc_I4, method.Body.Instructions[i].GetLdcI4Value() - sizes[lastIndex]));
                     method.Body.Instructions.Insert(i + 3, Instruction.Create(OpCodes.Ldc_I4, num));
                     method.Body.Instructions.Insert(i + 4, Instruction.Create(OpCodes.Ldc_I4, div));
                     method.Body.Instructions.Insert(i + 5, Instruction.Create(OpCodes.Xor));
@@ -57,12 +60,34 @@ namespace LoGiC.NET.Protections
                     method.Body.Instructions.Insert(i + 7, Instruction.Create(OpCodes.Bne_Un, nop));
                     method.Body.Instructions.Insert(i + 8, Instruction.Create(OpCodes.Ldc_I4, 2));
                     method.Body.Instructions.Insert(i + 9, localCode);
-                    method.Body.Instructions.Insert(i + 10, Instruction.Create(OpCodes.Sizeof, method.Module.Import(typeof(InsufficientMemoryException))));
+                    method.Body.Instructions.Insert(i + 10, Instruction.Create(OpCodes.Sizeof, method.Module.Import(randType)));
                     method.Body.Instructions.Insert(i + 11, Instruction.Create(OpCodes.Add));
                     method.Body.Instructions.Insert(i + 12, nop);
 
                     i += method.Body.Instructions.Count - i;
                 }
         }
+
+        private static readonly Type[] types = new Type[]
+        {
+            typeof(uint),
+            typeof(int),
+            typeof(long),
+            typeof(ulong),
+            typeof(ushort),
+            typeof(short),
+            typeof(double)
+        };
+
+        private static readonly int[] sizes = new int[]
+        {
+            sizeof(uint),
+            sizeof(int),
+            sizeof(long),
+            sizeof(ulong),
+            sizeof(ushort),
+            sizeof(short),
+            sizeof(double)
+        };
     }
 }
